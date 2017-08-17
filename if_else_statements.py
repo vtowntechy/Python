8fa705b44096996952daa28791609d156db01fb5
Ward Johnson <vtowntechy@gmail.com>
2017-08-17 11:33:31 -0700

    Update if_else_statements.py

--- if_else_statements.py
+++ if_else_statements.py
@@ -1,1 +1,6 @@
-# If Else Statements
+# If Else Statements Python
+if len(word) == 4:
+	print word + " is a four letter word. Careful now!"
+else:
+	print "Thats not a four letter word. Try harder!"
+
