b82697e7fff81a8b39c15e8e72108a6aefd11ad0
Ward Johnson <vtowntechy@gmail.com>
2017-08-17 16:55:14 -0700

    Create Name_lenght.py

---
+++ Name_lenght.py
@@ -0,0 +1,8 @@
+#/usr/bin/python
+#Bugs
+#Enter Code exactly as you see it(including comments like this one)
+#Never copy and paste code if possible.
+name = raw_input("Please enter your name: ")
+
+print "Hello " + name
+print "Your name is " str(len(name)) + "letters long."
