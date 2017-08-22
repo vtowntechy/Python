ae886fe405304287f3cb365850310a6c145a1899
Ward Johnson <vtowntechy@gmail.com>
2017-08-22 13:37:47 -0700

    Update if_else_statements.py

--- if_else_statements.py
+++ if_else_statements.py
@@ -1,6 +1,6 @@
 # If Else Statements Python
 word = raw_input("please enter a four letter word:")
 if len(word) == 4:
-	print word + " is a four letter word. Careful now!"
+	print word + " is a four letter word. You Did It!"
 else:
 	print "Thats not a four letter word. Try harder!"
